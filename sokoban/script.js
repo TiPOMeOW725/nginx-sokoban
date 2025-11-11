9e71a3b48419b2e29af9040d3ac7ec2871572e57
TiPOMeOW725 <gips8790@gmail.com>
2025-11-11 20:55:30 +0100

    Create key listener

--- sokoban/script.js
+++ sokoban/script.js
@@ -1,4 +1,4 @@
-map=["WWWWWWW",
+let map=["WWWWWWW",
     "W     W",
     "W TC  W",
     "WWTCWWW",
@@ -6,3 +6,16 @@ map=["WWWWWWW",
     "W TCW W",
     "W     W",
     "WWWWWWW"]; /* W = Wall  T = Target  P = Player  C = Crate   X = Crate on the target  O = Player on the target*/
+let key = ''
+let playerPos = [4,3]
+// Game cycle listen -> check -> update -> redraw
+
+function listen() {
+    document.addEventListener('keypress', (e) => {
+        key[e.key.toLowerCase()] = true;
+    });
+}
+
+function check() {
+
+}
