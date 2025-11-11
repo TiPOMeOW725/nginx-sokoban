f5c816b84c78951a793724b532da71820cf6ecc5
TiPOMeOW725 <gips8790@gmail.com>
2025-11-11 20:01:03 +0100

    Create map

---
+++ sokoban/script.js
@@ -0,0 +1,8 @@
+map=["WWWWWWW",
+    "W     W",
+    "W TC  W",
+    "WWTCWWW",
+    "W  P  W",
+    "W TCW W",
+    "W     W",
+    "WWWWWWW"]; /* W = Wall  T = Target  P = Player  C = Crate   X = Crate on the target  O = Player on the target*/
