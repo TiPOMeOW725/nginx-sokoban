5e2fa84a5719f4b51e0381fca5c744a5cfed56f5
TiPOMeOW725 <gips8790@gmail.com>
2025-11-13 16:58:50 +0100

    refine check for free space

--- sokoban/script.js
+++ sokoban/script.js
@@ -7,10 +7,10 @@ let map=["WWWWWWW",
     "W     W",
     "WWWWWWW"]; /* W = Wall  T = Target  P = Player  C = Crate   X = Crate on the target  O = Player on the target*/
 let key = {};
+let direction = '';
 let playerPos = [4,3]; // 1st = y    2nd = x
 let movePos=playerPos;//position user is trying to move to
-let isMoving=false;
-let isFree=false; //bool indicating if the next cell is free for moving
+let isMoving=false; // first bool indicating if the next cell is free for moving    the next one 
 // Game cycle listen -> check -> update -> redraw
 
 function listen() {
@@ -19,7 +19,7 @@ function listen() {
     });
 }
 
-function checkForFreeSpace(map,movePos,direction){
+function checkForFreeSpace(map,movePos,direction,isCrateBehind){
     let cell=map[movePos[0]][movePos[1]]
     switch (cell){
     case 'W':
@@ -29,6 +29,9 @@ function checkForFreeSpace(map,movePos,direction){
         return true;
     case 'C':
     case 'X':
+        if (isCrateBehind===true){
+            return false
+        }
         switch(direction){
             case 'w':
                 movePos[0]-=1
@@ -42,20 +45,44 @@ function checkForFreeSpace(map,movePos,direction){
             case 'd':
                 movePos[1]+=1
                 break
-        }
-        let beyondY=movePos[0], beyondX=movePos[1]
-        checkForFreeSpace(map,[beyondY,beyondX],direction)
+        }                                            
+        return checkForFreeSpace(map,movePos,direction,true)
     }
-    return true;
 }
+
+function move(map,movePos,playerPos){
+    let delta;
+    while(movePos[0]===playerPos[0] && movePos[1]===playerPos[1]){
+        if (movePos[0]!=playerPos[0]){
+            while(1){
+                delta=movePos[0]-playerPos[0]
+                map[movePos[0]][movePos[1]]=map[movePos[0]+delta][movePos[1]]}
+        }
+    }
+}
+
+
 
 function check() {
     switch(key){
         case 'w':
             movePos[0]-=1
-            isFree=checkForFreeSpace(map,movePos,'w')
+            direction='w'
+            isFree=checkForFreeSpace(map,movePos,direction,false)
+            if(isFree){
+
+            }
         case 's':
         case 'a':
         case 'd':
     }
 }
+
+// Your existing code above...
+
+// TEST SECTION - Remove later
+console.log("Testing checkForFreeSpace:");
+console.log("Player -> Empty:", checkForFreeSpace(map, [4, 4], 'd', false)); // should be true
+console.log("Player -> Wall:", checkForFreeSpace(map, [4, 0], 'a', false));  // should be false
+console.log("Player -> Crate -> Empty:", checkForFreeSpace(map, [2, 2], 'a', false)); // should be true?
+//console.log("Player -> Crate -> Crate:", checkForFreeSpace(map, [?, ?], '?', false)); // should be false
